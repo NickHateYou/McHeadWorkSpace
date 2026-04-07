2ea99db51084eb1bc7cf5f5b75920e329ccafdc9
Mykola Horishnyi <mykolahorishnyi@Mykolas-MacBook-Pro.local>
2026-04-07 17:22:04 +0300

    new file in javascri

--- js/core.js
+++ js/core.js
@@ -1,1 +1,19 @@
-console.log("JS loaded");
+let count = 0;
+
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.getElementById('clickBtn');
+  const text = document.getElementById('counterText');
+  const reset = document.getElementById('resetBtn');
+
+  if (button && text && reset) {
+    button.addEventListener('click', () => {
+      count++;
+      text.textContent = `You clicked ${count} times`;
+    });
+
+    reset.addEventListener('click', () => {
+      count = 0;
+      text.textContent = `You clicked 0 times`;
+    });
+  }
+});
